93e49b1c793a345a3819e9d462ade1e514319d4c
shahin <shahin.shahi1986@gmail.com>
2023-02-24 22:30:11 +0330

    make style

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 
 function addCard() {
-    
     const titletext = document.getElementById('titletext');
 
     const cardbox = document.querySelector('.cardbox');
@@ -26,6 +25,24 @@ function addCard() {
 
     newp.innerText = titletext.value;
 
+    const newparam1 = document.createElement('P');
+
+    newparam1.classList = 'user-id';
+
+    newparam1.innerText = titletext.value;
+
+    const newparam2 = document.createElement('P');
+
+    newparam2.classList = 'user-room';
+
+    newparam2.innerText = titletext.value;
+
+    const newparam3 = document.createElement('P');
+
+    newparam3.classList = 'user-floor';
+
+    newparam3.innerText = titletext.value;
+
     cardbox.appendChild(newcard);
 
     newcard.appendChild(newcardbody);
@@ -33,5 +50,12 @@ function addCard() {
     newcardbody.appendChild(newh3);
 
     newcardbody.appendChild(newp);
-  
+
+    newcardbody.appendChild(newparam1);
+
+    newcardbody.appendChild(newparam2);
+
+    newcardbody.appendChild(newparam3);
+
+
 }
