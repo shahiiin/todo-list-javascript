165d334eb9b5efa87539b1b5d590d0d5792eb7a1
shahin <shahin.shahi1986@gmail.com>
2023-02-25 23:01:20 +0330

    make style

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
 
 
 function addCard() {
+    
     const titletext = document.getElementById('titletext');
 
     const cardbox = document.querySelector('.cardbox');
 
-    const newcard = document.createElement('DIV');
+    const newcard = document.createElement('div');
 
     newcard.className = 'card';
 
-    const newcardbody = document.createElement('DIV');
+    const newcardbody = document.createElement('div');
 
     newcardbody.className = 'card-body';
 
-    const newh3 = document.createElement('H3');
+    const newh3 = document.createElement('h3');
 
     newh3.className = 'card-title';
 
     newh3.innerText = titletext.value;
 
-    const newp = document.createElement('P');
+    const newp = document.createElement('p');
 
     newp.classList = 'fontbig blue';
 
     newp.innerText = titletext.value;
 
-    const newparam1 = document.createElement('P');
+    const newparam1 = document.createElement('p');
 
     newparam1.classList = 'user-id';
 
     newparam1.innerText = titletext.value;
 
-    const newparam2 = document.createElement('P');
+    const newparam2 = document.createElement('p');
 
     newparam2.classList = 'user-room';
 
     newparam2.innerText = titletext.value;
 
-    const newparam3 = document.createElement('P');
+    const newparam3 = document.createElement('p');
 
     newparam3.classList = 'user-floor';
 
