f08e4dd6b9b8c11a13f5b77044d8fc0a09e43415
armin-bkh <arminbkh0921@yahoo.com>
2023-02-25 23:00:51 +0330

    bug fixed

--- script.js
+++ script.js
@@ -5,57 +5,51 @@ function addCard() {
 
     const cardbox = document.querySelector('.cardbox');
 
-    const newcard = document.createElement('DIV');
+    const newcard = document.createElement('div');
 
-    newcard.className = 'card';
+    newcard.classList.add('card');
 
-    const newcardbody = document.createElement('DIV');
+    const newcardbody = document.createElement('div');
 
-    newcardbody.className = 'card-body';
+    newcardbody.classList.add('card-body');
 
-    const newh3 = document.createElement('H3');
+    const newh3 = document.createElement('h3');
 
-    newh3.className = 'card-title';
+    newh3.classList.add('card-title');
 
     newh3.innerText = titletext.value;
 
-    const newp = document.createElement('P');
+    const newp = document.createElement('p');
 
-    newp.classList = 'fontbig blue';
+    newp.classList.add(['fontbig', 'blue']);
 
     newp.innerText = titletext.value;
 
-    const newparam1 = document.createElement('P');
+    const newparam1 = document.createElement('p');
 
-    newparam1.classList = 'user-id';
+    newparam1.classList.add('user-id');
 
     newparam1.innerText = titletext.value;
 
-    const newparam2 = document.createElement('P');
+    const newparam2 = document.createElement('p');
 
-    newparam2.classList = 'user-room';
+    newparam2.classList.add('user-room');
 
     newparam2.innerText = titletext.value;
 
-    const newparam3 = document.createElement('P');
+    const newparam3 = document.createElement('p');
 
-    newparam3.classList = 'user-floor';
+    newparam3.classList.add('user-floor');
 
     newparam3.innerText = titletext.value;
 
-    cardbox.appendChild(newcard);
-
-    newcard.appendChild(newcardbody);
-
     newcardbody.appendChild(newh3);
-
     newcardbody.appendChild(newp);
-
     newcardbody.appendChild(newparam1);
-
     newcardbody.appendChild(newparam2);
-
     newcardbody.appendChild(newparam3);
-
-
+    newcard.appendChild(newcardbody);
+    cardbox.appendChild(newcard);
 }
+
+
